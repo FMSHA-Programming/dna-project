c1bf9c2efe030d50da64ae96556b40b638b6271c
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2022-02-28 18:42:31 +0700

    random experiments impruved

--- experiment_random_dna.py
+++ experiment_random_dna.py
@@ -10,25 +10,34 @@ def are_rotation(line1, line2):
     return line1 in (line2 + line2)
 
 
-def run_random_assembly(dnalen, nreads, readlen, k, seed=31):
-    dna = random_dna_string(dnalen, seed=seed)
-    reads = random_reads(dna, nreads, readlen, seed=seed)
-    dnaa = DNAAssembler(k)
+def run_random_assembly(dnalen, nreads, readlen, k, cycle, rng):
+    dna = random_dna_string(dnalen, rng)
+    reads = random_reads(dna, nreads, cycle, readlen, rng)
+    dnaa = DNAAssembler(k, cycle, rng)
     result = dnaa(reads)
     print(f'{len(dnaa.g.nodes)} nodes')
     print(f'{len(dnaa.g.edges)} edges')
     dnaa.g.dump()
-    return dna, result, are_rotation(dna, result)
+    if cycle:
+        status = are_rotation(dna, result)
+    else:
+        status = dna == result
+    return dna, result, status
 
 
 def main():
-    assert len(sys.argv) == 5
-    dnalen, nreads, readlen, k = map(int, sys.argv[1:])
-    for seed in range(1):
-        dna, result, status = run_random_assembly(dnalen, nreads, readlen, k, seed=seed)
+    assert len(sys.argv) == 7
+    dnalen, nreads, readlen, k, seed, ntry = map(int, sys.argv[1:])
+    rng = np.random.default_rng(seed=seed)
+    nsuccess = 0
+    for idx in range(ntry):
+        dna, result, status = run_random_assembly(dnalen, nreads, readlen, k, rng)
         print(dna)
         print(result)
+        if status:
+            nsuccess += 1
         print(status)
+    print(f'Success rate: {nsuccess}/{ntry}')
 
 
 if __name__ == '__main__':
